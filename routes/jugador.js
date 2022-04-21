cfe8734dea3733375450398d4d1f4b12f0c748f1
Juan Pino <juanrespolo@gmail.com>
2022-04-21 11:11:27 -0500

    actualizar jugador

--- routes/jugador.js
+++ routes/jugador.js
@@ -1,13 +1,35 @@
-const router = require('express').Router();
-const Cryptojs = require("crypto-js")
-const Jugador = require('../models/jugador-model')
-const {verifyTokenAndAuth, verifyTokenAndAdmin} = require('../middlewares/verifyToken')
+const router = require("express").Router();
+const Cryptojs = require("crypto-js");
+const Jugador = require("../models/jugador-model");
+const {
+  verifyTokenAndAuth,
+  verifyTokenAndAdmin,
+} = require("../middlewares/verifyToken");
 
-router.get("/",verifyTokenAndAuth,async function(req, res) {
-    console.log("llega")
-    res.status(200).json("llego")
-})
+//Actualizar Jugador
+router.patch("/", verifyTokenAndAuth, async function (req, res) {
+  let user = req.body;
+  if (user.contrasena) {
+    user.contrasena = Cryptojs.AES.encrypt(
+      user.contrasena,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  if (!req.query.id) {
+    return res.status(500).json("No hay ID de usuario");
+  }
+  try {
+    const jugadorActualizado = await Jugador.findByIdAndUpdate(
+      req.query.id,
+      {
+        $set: user,
+      },
+      { new: true }
+    );
+    return res.status(200).json(jugadorActualizado);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
