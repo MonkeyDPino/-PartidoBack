b9c0ec1fa5c35cd3a4f92eb631754c8f1a91a3b1
Juan Pino <juanrespolo@gmail.com>
2022-04-21 11:47:45 -0500

    actualizar rol

--- routes/jugador.js
+++ routes/jugador.js
@@ -15,6 +15,9 @@ router.patch("/", verifyTokenAndAuth, async function (req, res) {
       process.env.PASS_SEC
     ).toString();
   }
+  if (user.rol) {
+    return res.status(500).json("No se puede cambiar rol");
+  }
   if (!req.query.id) {
     return res.status(500).json("No hay ID de usuario");
   }
@@ -31,5 +34,43 @@ router.patch("/", verifyTokenAndAuth, async function (req, res) {
     return res.status(500).json(err);
   }
 });
+
+//Actualizar Rol
+
+router.patch("/rol", verifyTokenAndAdmin, async function (req, res) {
+  const rol = req.body.rol;
+  if (!rol) {
+    return res.status(500).json("No hay ROL de usuario");
+  }
+  if (!req.query.id) {
+    return res.status(500).json("No hay ID de usuario");
+  }
+  try {
+    const jugadorActualizado = await Jugador.findByIdAndUpdate(
+      req.query.id,
+      {
+        $set: {
+          rol: rol,
+        },
+      },
+      { new: true }
+    );
+    return res.status(200).json(jugadorActualizado);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// router.delete("/",verifyTokenAndAdmin,async function(req, res){
+//     if(!req.query.id){
+//         return res.status(500).json("No hay ID de usuario");
+//     }
+//     try{
+//         await Jugador.findByIdAndDelete(req.query.id)
+//         res.status(200).json("Usuario eliminado")
+//     }catch (err) {
+//         return res.status(500).json(err)
+//     }
+// })
 
 module.exports = router;
