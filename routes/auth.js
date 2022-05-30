9674dc01cb41cf5e24de449c4738d4f22df2f156
Juan Pino <juanrespolo@gmail.com>
2022-05-30 13:05:42 -0500

    register update

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
-const router = require('express').Router();
-const Jugador = require('../models/jugador-model')
-const Cryptojs = require("crypto-js")
-const jwt = require('jsonwebtoken')
-const axios = require('axios');
-const AbstractApiKey = process.env.ABSTRACT_API_KEY
-
+const router = require("express").Router();
+const Jugador = require("../models/jugador-model");
+const Cryptojs = require("crypto-js");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const AbstractApiKey = process.env.ABSTRACT_API_KEY;
 
 //register
 router.post("/register", async function (req, response) {
@@ -39,85 +38,55 @@ router.post("/register", async function (req, response) {
     ).toString(),
   });
 
-  //Verficación de correo
-  axios.get(
-    "https://emailvalidation.abstractapi.com/v1/?api_key=" +
-      AbstractApiKey +
-      "&email=" +
-      user.correo
-  )
-  .then(async (res) => {
-    if (res.data.deliverability != "DELIVERABLE"){
-      return response.status(400).send({
-        ok: false,
-        error: "correo no válido",
-      });
-    }else{
-      //crear jugador
-      await newJugador.save((error, result) => {
-        if (error) {
-          return response.status(500).send({
-            ok: false,
-            error: error,
-          });
-        }
-        return response.send(result);
-      });
-    } 
-  })
-  .catch((error) => {
-    return response.status(400).send({
-      ok: false,
-      error: error,
-    });
-  });
-
-  
-});
-
-router.post("/login",async (req, response) => {
-    try{
-        const user = await Jugador.findOne({ correo: req.body.correo });
-      !user &&
-        response.status(401).send({
-          ok: false,
-          error: "El Usuario no existe",
-        });
-        
-
-      const Pass = Cryptojs.AES.decrypt(
-        user.contrasena,
-        process.env.PASS_SEC
-      ).toString(Cryptojs.enc.Utf8);
-
-      Pass !== req.body.contrasena &&
-        response.status(401).send({
-          ok: false,
-          error: "Malas Credenciales",
-        });
-
-      const accessToken = jwt.sign(
-        {
-          id: user._id,
-          rol: user.rol,
-        },
-        process.env.JWT_SEC,
-        { expiresIn: "1d" }
-      );
-
-      const { contrasena, ...others } = user._doc;
-
-      response.status(200).json({ ...others, accessToken });
-    }catch(err){
+  await newJugador.save((error, result) => {
+    if (error) {
       return response.status(500).send({
         ok: false,
-        error: err,
+        error: error,
       });
     }
-      
-    
-    
+    return response.send(result);
+  });
+});
 
-})
+router.post("/login", async (req, response) => {
+  try {
+    const user = await Jugador.findOne({ correo: req.body.correo });
+    !user &&
+      response.status(401).send({
+        ok: false,
+        error: "El Usuario no existe",
+      });
 
-module.exports = router
+    const Pass = Cryptojs.AES.decrypt(
+      user.contrasena,
+      process.env.PASS_SEC
+    ).toString(Cryptojs.enc.Utf8);
+
+    Pass !== req.body.contrasena &&
+      response.status(401).send({
+        ok: false,
+        error: "Malas Credenciales",
+      });
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        rol: user.rol,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "1d" }
+    );
+
+    const { contrasena, ...others } = user._doc;
+
+    response.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    return response.status(500).send({
+      ok: false,
+      error: err,
+    });
+  }
+});
+
+module.exports = router;
